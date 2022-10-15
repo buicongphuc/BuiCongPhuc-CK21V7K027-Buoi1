4f1e66617154bc87f7d2f5ceb646ca6aee47bed7
phucbui <61677435+buicongphuc@users.noreply.github.com>
2022-10-15 10:45:12 +0700

    Add files via upload

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -4,16 +4,16 @@ const contacts = require("../controllers/contact.controller");
 const router = express.Router();
 
 router.route("/")
-    .get(contacts.findAll)
-    .post(contacts.create)
-    .delete(contacts.deleteAll);
+.get(contacts.findAll)
+.post(contacts.create)
+.delete(contacts.deleteAll);
 
 router.route("/favorite")
-    .get(contacts.findAllFavorite);
+.get(contacts.findAllFavortie);
 
 router.route("/:id")
-    .get(contacts.findOne)
-    .post(contacts.update)
-    .delete(contacts.delete);
+.get(contacts.findOne)
+.put(contacts.update)
+.delete(contacts.delete);
 
 module.exports = router;
