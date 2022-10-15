0584d9871bf1b4703da7c43d2f909a687ed96e80
phucbui <61677435+buicongphuc@users.noreply.github.com>
2022-10-15 10:45:54 +0700

    Add files via upload

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,11 +1,10 @@
 const { MongoClient } = require("mongodb");
 
-class MongoDB{
+class MongoDB {
     static connect = async (uri) => {
         if (this.client) return this.client;
         this.client = await MongoClient.connect(uri);
         return this.client;
     };
 }
-
-module.exports =  MongoDB;
+module.exports = MongoDB;
